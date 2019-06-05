ad0928ddbd7543eb3f2a869844ead733114dbf74
haydenbaker <hkbaker@calpoly.edu>
2019-06-04 17:47:49 -0700

    query non-taken seats only

--- scripts/sql/search_seats.sql
+++ scripts/sql/search_seats.sql
@@ -1,12 +1,12 @@
 drop procedure if exists search_seats;
 delimiter $ 
-create procedure search_seats(in Fday datetime, Class enum('FIRST', 'BUSINESS', 'ECONOMY'), Location enum('WINDOW', 'MIDDLE', 'AISLE'), Passengers int, Src varchar(50), Dest varchar(50))
+create procedure search_seats(in Fday datetime, Class enum('FIRST', 'BUSINESS', 'ECONOMY'), Location enum('WINDOW', 'MIDDLE', 'AISLE'), Src varchar(50), Dest varchar(50))
 begin
-	select * from Flight f join Seat s using (fid) where 
+	select * from Flight f join Seat s using (fid) where
+		s.taken = 0 and
 		(Fday is NULL or date(f.FlightDate) = Fday) and
         (Class is NULL or s.Class = Class) and
         (Location is NULL or s.Location = Location) and
-		(Passengers is NULL) and
         (Src is NULL or f.SourceAirport = Src) and
         (Dest is NULL or f.DestAirport = Dest);
 end $
