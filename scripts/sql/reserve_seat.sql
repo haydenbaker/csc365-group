4b79c6c117658e0641cad609e21e8176d031f9af
Connor Graves <citygraves18@gmail.com>
2019-06-04 21:43:59 -0700

    Updated reserve_seat()
    better error handling, rollback support

--- scripts/sql/reserve_seat.sql
+++ scripts/sql/reserve_seat.sql
@@ -3,33 +3,31 @@ BEGIN
     DECLARE isTaken Integer DEFAULT NULL;
     DECLARE price INTEGER DEFAULT NULL;
     DECLARE credit_card INTEGER DEFAULT NULL;
+    DECLARE EXIT HANDLER FOR NOT FOUND SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'some value not found while attempting to reserve flight.';
     
     SELECT Taken INTO isTaken FROM Seat WHERE Sid = seat_id;
-    IF isTaken IS NULL THEN
-		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Seat does not exist.';
-	END IF;
     IF isTaken != 0 THEN
-		SIGNAL SQLSTATE '45000'
+		SIGNAL SQLSTATE '45001'
 		SET MESSAGE_TEXT = 'Seat has already been reserved.';
 	END IF;
     
     SELECT Cost INTO price FROM Seat WHERE Sid = seat_id;
-    IF price IS NULL THEN
-		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Pricing Error.';
-	END IF;
-    
     SELECT Cid INTO credit_card FROM Account WHERE AccId = acc_id;
-    IF credit_card IS NULL THEN
-		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Credit card not found.';
+
+    START TRANSACTION;
+    UPDATE Card SET Balance = Balance + price WHERE Cid = credit_card;
+    IF row_count() != 1 THEN
+		ROLLBACK;
 	END IF;
     
-    START TRANSACTION;
-    SET AUTOCOMMIT = 0;
-    UPDATE Card SET Balance = Balance + price WHERE Cid = credit_card;
     UPDATE Seat SET Taken = 1 WHERE Sid = seat_id;
+    IF row_count() != 1 THEN
+		ROLLBACK;
+	END IF;
+    
     INSERT INTO Reservation(Rdate, Sid, AccId) VALUES(NOW(), seat_id, acc_id);
+    IF row_count() != 1 THEN
+		ROLLBACK;
+	END IF;
     COMMIT;
 END
